6ba656f9cc049c57b313386dee90cf72bdaa8a83
Matt R <>
2020-05-17 22:03:11 +0100

    first commit of cra based app

---
+++ precache-manifest.7293d559178daa36c9ee3611d9ea44cb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f2dfdb30e0fbd7a4ede9c9b3a2293b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b1b363c7ef9f7b198a",
+    "url": "/static/css/main.a86cca69.chunk.css"
+  },
+  {
+    "revision": "c81f42e292e2a40a6022",
+    "url": "/static/js/2.892e2f39.chunk.js"
+  },
+  {
+    "revision": "be14b386ccc34a49dee76740150eacbc",
+    "url": "/static/js/2.892e2f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b1b363c7ef9f7b198a",
+    "url": "/static/js/main.c1069bfd.chunk.js"
+  },
+  {
+    "revision": "f12c4fe935d2caa450c5",
+    "url": "/static/js/runtime-main.948ac377.js"
+  },
+  {
+    "revision": "561ade2c8f95d045852c7bacc1f763f6",
+    "url": "/static/media/banner.561ade2c.png"
+  },
+  {
+    "revision": "3b03abd8b615e101b7551d4259d8d1e7",
+    "url": "/static/media/grounded.3b03abd8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49e5666447f92f6c7ad5e00efb1c24fc",
+    "url": "/static/media/ost.49e56664.png"
+  },
+  {
+    "revision": "8f11547f8401ced454c5c2c73b15c86e",
+    "url": "/static/media/pikalion.8f11547f.png"
+  },
+  {
+    "revision": "9d683bd7bec9a51f29aa1827a4af5ded",
+    "url": "/static/media/piktobw.9d683bd7.png"
+  },
+  {
+    "revision": "a975093f855c5000cbc983593a09c745",
+    "url": "/static/media/piktocol.a975093f.png"
+  },
+  {
+    "revision": "d493261c67c23139942e032c47151967",
+    "url": "/static/media/platformj.d493261c.png"
+  },
+  {
+    "revision": "2c3746355d798a90a012f969540e7fd6",
+    "url": "/static/media/runaround.2c374635.png"
+  },
+  {
+    "revision": "4b932ddd043430a53d7abc00e48fca89",
+    "url": "/static/media/runningmiss.4b932ddd.png"
+  },
+  {
+    "revision": "74ebc8d5ec7204fc492d641b445f9a0b",
+    "url": "/static/media/solidsounds.74ebc8d5.png"
+  }
+]);
